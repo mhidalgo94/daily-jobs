a8ebf537744f92bbb9a76ce522700307c5b28ad7
mhidalgo94 <mhidalgodev@outlook.com>
2025-02-13 00:27:38 -0500

    Add ListJobs items

--- components/tabs/home.jsx
+++ components/tabs/home.jsx
@@ -11,7 +11,7 @@ export const HeadHome = ()=> {
         <View className="flex flex-row items-center justify-between pt-2">
             <View>
                 <Text className="text-base font-rubik text-black-300">Welcome</Text>
-                <Text className="text-2xl font-rubik-medium text-primary">{user.name}</Text>
+                <Text className="text-2xl font-rubik-medium text-black-250">{user.name}</Text>
                 <View className="my-1 flex flex-row items-center gap-2 w-[100px]" >
                     <Ionicons name="sparkles" size={18} color = '#191D31'/>
                     <Text className="text-base">Plan - {plan} </Text>
@@ -45,6 +45,49 @@ export const CardHome = ()=>{
                     <Text className="text-black-200 mt-2 pr-8 font-rubik-medium">{utc}</Text>
                 </View>
             </View>  
+        </View>
+    )
+}
+
+
+export const ListJobs = ({job})=>{
+    const IconJobs = ({category}) => {
+        if( category == "New Install"){
+            return (
+                // <View className="bg-black-100 p-2 rounded rounded-xl4">
+                    <Ionicons name="bag-add" size={34} color="#4b5563"/>
+                // </View>
+            )
+        } else if (category == "Service Change"){
+            return <Ionicons name="bag-check" size={38} color="#4b5563"/>
+        } else if (category == "Trouble Call"){
+            return <Ionicons name="construct" size={38} color="#4b5563"/>
+        }else if (category == "Special Request"){
+            return <Ionicons name="warning" size={38} color="#4b5563"/>
+        } else{
+            return <Ionicons name="bag-add" size={38} color="#4b5563"/>
+        }
+    }
+
+    const StatusText = ({status})=>{
+        if(status== "Complete"){
+
+        }
+    }
+    return (
+        <View className="mt-2 px-2 pt-2 pb-3 border-b-[1px] border-gray-100">
+            <View className="flex flex-row gap-3 items-center justify-between">
+                <View className="flex flex-row gap-3">
+                    <IconJobs category={job.category} />
+                    <View>
+                        <Text className="text-2xl font-rubik-medium text-black-300">{job.id}</Text>
+                        <Text className="text-black-300">{job.address}</Text>
+                    </View>
+                </View>
+                <View>
+                    <Text className="text-regular font-rubik-medium" >{job.status}</Text>
+                </View>
+            </View>
         </View>
     )
 }
